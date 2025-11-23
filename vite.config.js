e51c930e6bdb8762fea61e3615439dd7086a095e
jarodlopez <127226881+jarodlopez@users.noreply.github.com>
2025-11-23 17:30:24 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  // Plugin necesario para que Vite procese y use React/JSX
   plugins: [react()],
-})
+  // Esto es crucial para asegurar que las rutas se resuelvan correctamente
+  base: '/',
+  // Esto puede ayudar a resolver conflictos de capitalización y rutas
+  resolve: {
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
+  }
+});
